e64d7eb22aa60de8602f36888a8228dd377da9df
shenyu <552395285@qq.com>
2019-03-11 18:07:18 +0800

    集成layui前端页面

---
+++ src/main/java/cn/swallow/platform/modular/api/OpenApiController.java
@@ -0,0 +1,8 @@
+package cn.swallow.platform.modular.api;
+
+/**
+ * @author shenyu
+ * @create 2019/3/11
+ */
+public class OpenApiController {
+}
---
+++ src/main/java/cn/swallow/platform/modular/system/constant/UserAdminState.java
@@ -0,0 +1,8 @@
+package cn.swallow.platform.modular.system.constant;
+
+/**
+ * @author shenyu
+ * @create 2019/3/11
+ */
+public class UserAdminState {
+}
