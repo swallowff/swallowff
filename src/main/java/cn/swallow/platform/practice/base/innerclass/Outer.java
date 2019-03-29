ddc175c013b88ba15286ded67bd161f6f7e80f86
shenyu <552395285@qq.com>
2019-03-30 00:13:31 +0800

    学习静态内部类

--- src/main/java/cn/swallow/platform/practice/base/innerclass/Outer.java
+++ src/main/java/cn/swallow/platform/practice/base/innerclass/Outer.java
@@ -10,9 +10,14 @@ public class Outer {
 
     }
 
+    private static void outerStaticMethod(){
+
+    }
+
     /**
      * 成员内部类
      * 成员内部类是依附外部类而存在的，也就是说，如果要创建成员内部类的对象，前提是必须存在一个外部类的对象
+     * 成员内部类不能拥有静态属性和静态方法
      */
     public class Inner{
         private String innerProp;
@@ -28,7 +33,8 @@ public class Outer {
             String a = outerProp;
             outerMethod();
 
-            String s = Outer.this.outerProp; //如果内部类拥有和外部类相同的成员和方法时,外部类的成员和方法会被隐藏,需要使用this关键字来访问
+            //如果内部类拥有和外部类相同的成员和方法时,外部类的成员和方法会被隐藏,需要使用this关键字来区分
+            String s = Outer.this.outerProp;
             String x = Inner.this.innerProp;
         }
     }
@@ -44,12 +50,24 @@ public class Outer {
     /**
      * 静态内部类
      * 静态内部类不需要依附外部对象,但是不能访问外部类的非静态成员变量和非静态方法
+     * 静态内部类可以拥有静态属性和静态方法
      */
     public static class StaticInner{
+        private String staticInnerProp;
+
+        //静态内部类可以拥有静态属性
+        private static String innerProp;
 
         public void getOuterProp(){
-            //静态内部类只能访问外部类的静态成员
+            //静态内部类只能访问外部类的静态成员和静态方法
             String a = outerStaticProp;
+            outerStaticMethod();
+
+        }
+
+        //静态内部类可以拥有静态方法,外部可以直接访问
+        private static void staticInnerMethod(){
+
         }
 
 
@@ -62,5 +80,6 @@ public class Outer {
 
         //静态内部类可以不依附于外部类对象
         StaticInner staticInner = new StaticInner();
+        String a = StaticInner.innerProp;
     }
 }
